7fa4064589f92512279e25f6cc4504644119020b
ZPH <Zander@civet.ws>
2013-07-28 15:36:28 -0400

    Improved gsub method and properly includes /usr/ files

--- lib/valt.rb
+++ lib/valt.rb
@@ -2,22 +2,64 @@ require "valt/version"
 require "thor"
 require "yaml"
 require "yaml/store"
+require "pathname"
+require "fileutils"
 
 #TODO: set hook in ~/.bashrc, ~/.zshrc that sources ~/.valt.d/valt.sh if exists
 #TODO: create ~/.valt.d/valt.sh to check ~/.valtrc for which directory and source files there
 #TODO: ~/.valt.d/zph/.valt_local should prepend ~/.valt.d/profile/bin onto $PATH
 
+
 module Valt
   class CLI < Thor
 
     desc "profile_name", "use alternate vim configs"
-    def init(path)
-      path ||= File.expand_path("~/.valtrc")
-      puts "We'll put init file at #{path}"
-      #TODO: init structure
-      # create ~/.valtrc
-      # create ~/.valt.d
-      # fill basic hooks in ~/.valt.d
+    def init(path="~/")
+      valtrc = File.expand_path("#{path}.valtrc")
+      if File.exists?(valtrc)
+        puts "Valtrc found, please remove or manually copy valtrc"
+      else
+        Helper.install_valtrc(path)
+      end
+
+      valt_d = File.expand_path("#{path}.valt.d")
+      if File.exists?(File.expand_path("#{path}.valt.d") )
+        puts "Valt.d found, please remove or manually copy valt.d"
+      else
+        Helper.install_valt_d(path)
+      end
+    end
+
+    desc "install profile dotfile_repo", "install new profile from git repo"
+    def install(profile, url)
+      puts username
+      Dir.mkdir(File.expand_path("~/.valt.d/#{profile}"))
+
+      puts url
+      Dir.mktmpdir do |dir|
+        #TODO: accoutnf or submodules
+        Dir.chdir(dir) do
+          # URI.parse(url)
+          `\git clone #{url} dotfiles`
+          Dir.chdir("dotfiles") do
+
+            `\git submodule init`
+            `\git submodule update`
+            vimrc = Dir.glob("#{dir}/**/.vimrc")[0]
+            vim_dir = Dir.glob("#{dir}/**/.vim")[0]
+
+            valt_d_profile = File.expand_path("~/.valt.d/#{profile}")
+
+            # [vimrc, vim_dir].each do |resource|
+            #   FileUtils.copy(resource, valt_d_profile)
+            # end
+            FileUtils.cp(vimrc, valt_d_profile)
+            FileUtils.cp_r(vim_dir, valt_d_profile)
+
+          end
+        end
+      end
+
     end
 
     desc "list_profiles", "show all profiles"
@@ -38,31 +80,56 @@ module Valt
     end
   end
 
+  module Helper
+    extend self
+
+      def install_valtrc(path)
+        valtrc = "#{path}.valtrc"
+        valtrc_path = File.expand_path(valtrc)
+        puts "We'll put init file at #{path}"
+        puts "Installing #{valtrc}"
+        config = Valt::Config.create_valtrc
+      end
+
+      def install_valt_d(path)
+        valt_d = "#{path}.valt.d"
+        valt_d_path = File.expand_path(valt_d)
+        puts "Installing ~/.valt.d/"
+        current_path = Pathname.new(File.expand_path(File.join(__FILE__, ".."))).dirname
+        unless Dir.exists?(valt_d_path)
+          FileUtils.cp_r( "#{current_path.to_s}/templates/.valt.d" , File.expand_path(valt_d_path) )
+        end
+      end
+  end
+
   class Config
     attr_accessor :content
 
     def self.retreive
       if File.exists? File.expand_path("~/.valtrc")
-        config = valtrc_path
+        @content = YAML.load_file(valtrc_path)
       else
-        File.write(valtrc_path, defaults.to_yaml)
+        puts "No .valtrc found"
       end
-
-      config ||= valtrc_path
-      @content = YAML.load_file(config)
     end
 
     def self.valtrc_path
       File.expand_path("~/.valtrc")
     end
 
+    def self.create_valtrc(path="~/")
+      valtrc_path = File.expand_path File.join(path, ".valtrc")
+      File.write(valtrc_path, defaults.to_yaml)
+    end
+
     def self.defaults
-      {basename: ".valtrc",
+      {
+       username: ENV['USER'],
+       basename: ".valtrc",
        path: File.expand_path("~"),
        full_path: File.expand_path(File.join("~", ".valtrc")),
        valt_d_path: File.expand_path("~/.valt.d"),
-       username: ENV['USER'],
-       version: "0.0.1",
+       version: Valt::VERSION,
       }
     end
   end
--- templates/.valt.d/valt.sh
+++ templates/.valt.d/valt.sh
@@ -1,10 +1,3 @@
-# Exists at ~/.valt.d/valt.sh
-# To enable valt functionality, add the following to .bashrc or .zshrc:
-########################################BEGIN
-# if [ -f "$HOME/.valt.d/valt.sh" ]; then
-#    source ~/.valt.d/valt.sh
-# fi
-########################################END
 username=$(awk '$0 ~ /username/ {print $2}' ~/.valtrc)
 valt_d_path=$(awk '$0 ~ /valt_d/ {print $2}' ~/.valtrc)
 
